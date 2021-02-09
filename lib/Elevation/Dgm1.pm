685f44a89872d32dc9a5dec45691843e5f35a28f
Tjabo Vierbuecher <tv@taponet.de>
2021-02-09 21:56:36 +0100

    Broken: trying to load on demand and to get elevations. Need test subs.

--- lib/Elevation/Dgm1.pm
+++ lib/Elevation/Dgm1.pm
@@ -23,6 +23,7 @@ use strict;
 use warnings;
 use Math::Round;
 use Geo::Coordinates::UTM qw(latlon_to_utm);
+use Data::Dumper;
 
 sub new
 {
@@ -33,6 +34,8 @@ sub new
 		tmp_dir => $tmp_dir,
 		correction => $correction,
 		elevations => {},
+		loaded_files => {},
+		tests => 0,
 	}, $class;
 
 }
@@ -41,20 +44,51 @@ sub ll
 {
 	my( $self, $model, $lat, $lon ) = @_;
 
+	if ($self->{ tests } == 0)
+	{
+		$self->{ tests } = 1;
+		$self->load_dgm1_file( "dgm1_test.xyz" );
+		print Dumper( $self->{ elevations } );
+	}
+
 	my ($zone, $east, $north) = latlon_to_utm( 23, $lat, $lon );
 	$east = round($east);   # aka Rechtswert
 	$north = round($north); # aka Hochwert
 	print "lat=$lat, lon=$lon -> z $zone, east=$east, north=$north\n";
-	my $key = $self->get_dgm1_key( $zone, $east, $north );
-	if (! defined( $self->{ elevations }->{ $key } ))
+
+	# read dgm1 file if data not present:
+	my $dgm1file = $self->get_dgm1_filename( $zone, $east, $north );
+	if (! $self->{ loaded_files }->{ $dgm1file } )
 	{
-		$self->load_dgm1( $key ) or die("cannot load dgm1 data from '$key'\n");
+		$self->load_dgm1_file( $dgm1file ) or die("cannot load dgm1 data from '$dgm1file'\n");
 	}
-	return 60;
+
+	# get elevation of dot:
+	# TODO: map height into minheight and maxheight of area, needs more options?
+	my $minheight = 220; # m
+	my $maxheight = 492; # m
+
+	my $dotkey = $self->get_dotkey( $zone, $east, $north );
+	my $elev = $self->{ elevations }->{ $dotkey };
+	if (defined($elev) and ($elev >= $minheight) && ($elev <= $maxheight))
+	{
+		my $xelev = 60 + (90 * ($elev - $minheight) / ($maxheight - $minheight));
+		$xelev = round($xelev);
+		print "elev=$elev, xelev=$xelev\n";
+		$xelev = 250 if $xelev > 250;
+		$xelev = 0 if $xelev < 0;
+		$elev = $xelev;
+	}
+	else {
+		$elev = 60; # default
+		print "no elev, using 60\n";
+	}
+
+	return $elev;
 }
 
 # Generate internal key AND filename where to find elevation data for $zone,$east,$north
-sub get_dgm1_key
+sub get_dgm1_filename
 {
 	my ($self, $zone, $east, $north) = @_;
 	if ($zone =~ /^([0-9]+)/) {
@@ -71,8 +105,18 @@ sub get_dgm1_key
 	return "dgm1_" . $zone . $east . "_" . $north . "_2_nw.xyz";
 }
 
+sub get_dotkey
+{
+	my ($self, $zone, $east, $north) = @_;
+	if ($zone =~ /^[0-9]+/) {
+		$zone = $1;
+	}
+
+	return $zone . round($east) . "_" . round($north);
+}
+
 # Load dgm1...xyz file into $self->elevation
-sub load_dgm1
+sub load_dgm1_file
 {
 	my ($self, $filename) = @_;
 	print "Loading $filename ... ";
@@ -83,10 +127,11 @@ sub load_dgm1
 	{
 		chomp($line);
 		my @parts = split(/ +/, $line);
-		my $dotkey = $parts[0] . "_" . $parts[1];
-		$self->{ elevations }->{ $filename }->{ $dotkey } = $parts[2];
+		my $dotkey = round( $parts[0] ) . "_" . round( $parts[1] );
+		$self->{ elevations }->{ $dotkey } = $parts[2]; # float
 	}
 	close($fh);
+	$self->{ loaded_files }->{ $filename } = 1;
 	print "done\n";
 	return 1;
 }
